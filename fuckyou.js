edc8a2c04b6e6ddd12cae7964aa2b81841a1db4d
Garrison Jensen <garrison.jensen@gmail.com>
2017-01-04 23:29:53 -0800

    Remove iife, it's too much.

--- fuckyou.js
+++ fuckyou.js
@@ -1,31 +1,34 @@
-(function(){
-  var url = window.location.href;
-  var name = url.split("?")[1];
-  if (typeof name === 'undefined') {
+var url = window.location.href;
+var name = url.split("?")[1];
+
+if (typeof name === 'undefined') {
     removeBackground();
     var exampleUrl = url + "?Steve_Smith";
-    setTitle("Hi :)")
+    setTitle("Hi :)");
     setMessage("Add a name to the end of the url like this: <p><a href='" + exampleUrl + "'>" + exampleUrl + "</a></p>");
-  } else {
+} else {
     setTitle("Fuck You, " + name.replace("_", " "));
     setMessage("<p class='rainbow big_text'>Fuck You, " + name.replace("_", " ") + "</p>");
     startTextAnimation();
-  }
-  function startTextAnimation() {
+}
+
+function startTextAnimation() {
     document.getElementById("special_message_for").classList.add("rainbow-shadow");
     var toggleMove = function() {
-      document.getElementById("special_message_for").classList.toggle("move");
-      setTimeout(toggleMove, 700);
+        document.getElementById("special_message_for").classList.toggle("move");
+        setTimeout(toggleMove, 700);
     };
     toggleMove();
-  }
-  function setTitle(title) {
+}
+
+function setTitle(title) {
     document.title = title;
-  }
-  function setMessage(message) {
+}
+
+function setMessage(message) {
     document.getElementById("special_message_for").innerHTML = message;
-  }
-  function removeBackground() {
+}
+
+function removeBackground() {
     document.body.style.background = 'none';
-  }
-})();
+}
