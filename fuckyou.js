36284f078bc593f9e8ab3a4e10f633de4d926df6
Garrison Jensen <garrison.jensen@gmail.com>
2017-01-04 23:19:29 -0800

    Remove marquee fingers, those are dumb.

--- fuckyou.js
+++ fuckyou.js
@@ -7,7 +7,6 @@
     setTitle("Hi :)")
     setMessage("Add a name to the end of the url like this: <p><a href='" + exampleUrl + "'>" + exampleUrl + "</a></p>");
   } else {
-    addMarqueeFingers();
     setTitle("Fuck You, " + name.replace("_", " "));
     setMessage("<p class='rainbow big_text'>Fuck You, " + name.replace("_", " ") + "</p>");
     startTextAnimation();
@@ -28,20 +27,5 @@
   }
   function removeBackground() {
     document.body.style.background = 'none';
-  }
-  function addMarqueeFingers() {
-    var marquee_element = document.getElementById("marquee_fingers");
-    var speeds = ["50","75","25","150"]
-    var directions = ["right","left","right","left"]
-    for(var i=0; i <= 3; i=i+1){
-      var middleFingerEmoji = document.createElement("img");
-      middleFingerEmoji.src = "middleFingerEmoji.png"
-      var marquee = document.createElement("div")
-      marquee.appendChild(middleFingerEmoji);
-      marquee.classList.add("marquee_finger");
-      marquee.style.top = (200*i) + "px";
-      marquee_element.appendChild(marquee);
-      console.log(marquee);
-    }
   }
 })();
